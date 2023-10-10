061a14de44de895810c21b0a8697a99b2f2841e9
Ben Ross <mrbenross@gmail.com>
2023-10-10 17:24:25 -0500

    Todojs updated

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 export default class Todo extends React.Component {
   render() {
+    const {id, name, completed} = this.props.todo
+
     return (
       <div>
-        Todo
+        {name} {completed && ' ✅'}
       </div>
     )
   }
